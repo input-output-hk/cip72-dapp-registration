55243f88f5e58c079b5e3a81c60a76673c99776a
John Oshalusi <john.oshalusi@iohk.io>
2023-12-04 14:23:54 +0100

    feat: fetch and validate metadata

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import inquirer from "inquirer";
 import chalk from "chalk";
 import figlet from "figlet";
 import _ from "lodash";
 
-import { calculateRootHash, generateMetadataJsonFile } from "./jsonGenerator.js"
+import {
+  calculateRootHash,
+  generateMetadataJsonFile,
+} from "./jsonGenerator.js";
 import {
   queryUTXO,
   createDraftTransaction,
@@ -14,7 +17,8 @@ import {
   buildRealTransaction,
   signedRealTransaction,
   submitTransaction,
-  cleanupTransactionFiles, getSignedTxTransactionId
+  cleanupTransactionFiles,
+  getSignedTxTransactionId,
 } from "./submitTransaction.js";
 
 dotenv.config();
@@ -22,17 +26,16 @@ dotenv.config();
 const METADATA_FILE_PATH = "metadata.json";
 
 const walletAddress = process.env.WALLET_ADDRESS;
-const cipJsonFilePath = process.env.CIP_JSON_FILE_PATH;
 const protocolFilePath = process.env.PROTOCOL_FILE_PATH;
 const paymentSkeyFilePath = process.env.PAYMENT_SKEY_FILE_PATH;
 const net = process.env.NET;
 const PAD_END_SIZE = 150;
 
-Object.defineProperty(String.prototype, 'capitalize', {
+Object.defineProperty(String.prototype, "capitalize", {
   value: function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
   },
-  enumerable: false
+  enumerable: false,
 });
 
 const init = () => {
@@ -41,18 +44,18 @@ const init = () => {
       figlet.textSync(`CIP-72 CLI`, {
         // font: "Ghost",
         horizontalLayout: "default",
-        verticalLayout: "default"
-      })
-    )
+        verticalLayout: "default",
+      }),
+    ),
   );
 };
 
 const askQuestions0 = () => {
   const questions = [
     {
-      type: 'list',
-      name: '_net',
-      message: 'Which network do you want to operate on?',
+      type: "list",
+      name: "_net",
+      message: "Which network do you want to operate on?",
       choices: ["Devnet", "Preview", "Preprod", "Mainnet"],
       default: net ? net.capitalize() : "Mainnet",
       filter(val) {
@@ -66,22 +69,16 @@ const askQuestions0 = () => {
 const askQuestions1 = () => {
   const questions = [
     {
-      name: "_cipJsonFilePath",
-      type: "input",
-      message: "CIP-72 json path/file-name?",
-      default: cipJsonFilePath
-    },
-    {
-      type: 'list',
-      name: '_actionType',
-      message: 'What action is the certificate asserting?',
+      type: "list",
+      name: "_actionType",
+      message: "What action is the certificate asserting?",
       choices: ["REGISTER", "DE_REGISTER"],
-      default: "REGISTER"
+      default: "REGISTER",
     },
     {
       name: "_comment",
       type: "input",
-      message: "Comment your change (optional):"
+      message: "Comment your change (optional):",
     },
     {
       name: "_metadataUrl",
@@ -98,105 +95,215 @@ const askQuestions2 = () => {
       name: "_walletAddress",
       type: "input",
       message: "Wallet address?",
-      default: walletAddress
+      default: walletAddress,
     },
     {
       name: "_protocolFilePath",
       type: "input",
       message: "Protocol file?",
-      default: protocolFilePath
+      default: protocolFilePath,
     },
     {
       name: "_paymentSkeyFilePath",
       type: "input",
       message: "Payment skey file?",
-      default: paymentSkeyFilePath
+      default: paymentSkeyFilePath,
     },
-
   ];
   return inquirer.prompt(questions);
 };
 
-
 const run = async () => {
-
   try {
     // show script introduction
     init();
 
     // ask questions1: CIP-26 json file generation
-    console.log(chalk.black.bgMagenta.bold(_.padEnd('-', PAD_END_SIZE, '-')))
-    console.log(chalk.black.bgMagenta.bold(_.pad("Choose network", PAD_END_SIZE)));
-    console.log(chalk.black.bgMagenta.bold(_.padEnd('-', PAD_END_SIZE, '-')))
+    console.log(chalk.black.bgMagenta.bold(_.padEnd("-", PAD_END_SIZE, "-")));
+    console.log(
+      chalk.black.bgMagenta.bold(_.pad("Choose network", PAD_END_SIZE)),
+    );
+    console.log(chalk.black.bgMagenta.bold(_.padEnd("-", PAD_END_SIZE, "-")));
     const { _net } = await askQuestions0();
 
     // ask questions1: Metadata.json generation
     console.log();
-    console.log(chalk.yellowBright.bgBlue.bold(_.padEnd('-', PAD_END_SIZE, '-')))
-    console.log(chalk.yellowBright.bgBlue.bold(_.pad("Metadata.json generation", PAD_END_SIZE)));
-    console.log(chalk.yellowBright.bgBlue.bold(_.padEnd('-', PAD_END_SIZE, '-')))
+    console.log(
+      chalk.yellowBright.bgBlue.bold(_.padEnd("-", PAD_END_SIZE, "-")),
+    );
+    console.log(
+      chalk.yellowBright.bgBlue.bold(
+        _.pad("Metadata.json generation", PAD_END_SIZE),
+      ),
+    );
+    console.log(
+      chalk.yellowBright.bgBlue.bold(_.padEnd("-", PAD_END_SIZE, "-")),
+    );
     const answers1 = await askQuestions1();
-    const { _actionType, _comment, _metadataUrl, _cipJsonFilePath } = answers1;
-    const _rootHash = calculateRootHash(_cipJsonFilePath)
-    console.log(chalk.yellowBright.bgBlue.bold(_.pad(`Calculated rootHash: ${_rootHash}`, PAD_END_SIZE)))
-    const out2 = generateMetadataJsonFile(_cipJsonFilePath, METADATA_FILE_PATH, _actionType, _comment, _metadataUrl, _rootHash)
-    if (out2 === true) console.log(chalk.yellowBright.bgBlue.bold(_.pad(`Metadata.json generated: ${METADATA_FILE_PATH}`, PAD_END_SIZE)))
+    const { _actionType, _comment, _metadataUrl } = answers1;
+    const { rootHash, metadata } = await calculateRootHash(_metadataUrl);
+    console.log(
+      chalk.yellowBright.bgBlue.bold(
+        _.pad(`Calculated rootHash: ${rootHash}`, PAD_END_SIZE),
+      ),
+    );
+    const out2 = await generateMetadataJsonFile(
+      METADATA_FILE_PATH,
+      _actionType,
+      _comment,
+      _metadataUrl,
+      rootHash,
+      metadata,
+    );
+    if (out2 === true)
+      console.log(
+        chalk.yellowBright.bgBlue.bold(
+          _.pad(`Metadata.json generated: ${METADATA_FILE_PATH}`, PAD_END_SIZE),
+        ),
+      );
 
     // ask questions2: on-chain submission
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd('-', PAD_END_SIZE, '-')))
-    console.log(chalk.black.bgGreenBright.bold(_.pad("Block-chain submission", PAD_END_SIZE)));
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd('-', PAD_END_SIZE, '-')))
+    console.log(
+      chalk.black.bgGreenBright.bold(_.padEnd("-", PAD_END_SIZE, "-")),
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.pad("Block-chain submission", PAD_END_SIZE),
+      ),
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(_.padEnd("-", PAD_END_SIZE, "-")),
+    );
     const answers4 = await askQuestions2();
-    const { _walletAddress, _protocolFilePath, _paymentSkeyFilePath } = answers4;
+    const { _walletAddress, _protocolFilePath, _paymentSkeyFilePath } =
+      answers4;
     const { TxHash, TxIx, Amount } = await queryUTXO(_walletAddress, _net);
 
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Last UTXO TxHash: ${TxHash}`, PAD_END_SIZE)))
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Last UTXO TxIx: ${TxIx}`, PAD_END_SIZE)))
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Wallet amount: ${Amount}`, PAD_END_SIZE)))
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Last UTXO TxHash: ${TxHash}`, PAD_END_SIZE),
+      ),
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Last UTXO TxIx: ${TxIx}`, PAD_END_SIZE),
+      ),
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Wallet amount: ${Amount}`, PAD_END_SIZE),
+      ),
+    );
 
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`Creating transaction draft...`, PAD_END_SIZE)))
-    await createDraftTransaction(_walletAddress, METADATA_FILE_PATH, TxHash, TxIx)
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Transaction draft created!`, PAD_END_SIZE)))
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`Creating transaction draft...`, PAD_END_SIZE),
+      ),
+    );
+    await createDraftTransaction(
+      _walletAddress,
+      METADATA_FILE_PATH,
+      TxHash,
+      TxIx,
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Transaction draft created!`, PAD_END_SIZE),
+      ),
+    );
 
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`Calculating transaction fee...`, PAD_END_SIZE)))
-    const { fee, finalAmount } = await calculateTransactionFee(_protocolFilePath, Amount, _net);
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Fee: ${fee}`, PAD_END_SIZE)))
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Final wallet amount: ${finalAmount}`, PAD_END_SIZE)))
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`Calculating transaction fee...`, PAD_END_SIZE),
+      ),
+    );
+    const { fee, finalAmount } = await calculateTransactionFee(
+      _protocolFilePath,
+      Amount,
+      _net,
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(_.padEnd(`- Fee: ${fee}`, PAD_END_SIZE)),
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Final wallet amount: ${finalAmount}`, PAD_END_SIZE),
+      ),
+    );
 
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`Building transaction...`, PAD_END_SIZE)))
-    await buildRealTransaction(_walletAddress, METADATA_FILE_PATH, TxHash, TxIx, fee, finalAmount);
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Transaction built!`, PAD_END_SIZE)))
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`Building transaction...`, PAD_END_SIZE),
+      ),
+    );
+    await buildRealTransaction(
+      _walletAddress,
+      METADATA_FILE_PATH,
+      TxHash,
+      TxIx,
+      fee,
+      finalAmount,
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Transaction built!`, PAD_END_SIZE),
+      ),
+    );
 
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`Signing transaction...`, PAD_END_SIZE)))
-    await signedRealTransaction(_paymentSkeyFilePath, _net)
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Transaction signed!`, PAD_END_SIZE)))
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`Signing transaction...`, PAD_END_SIZE),
+      ),
+    );
+    await signedRealTransaction(_paymentSkeyFilePath, _net);
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Transaction signed!`, PAD_END_SIZE),
+      ),
+    );
 
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`Submitting transaction...`, PAD_END_SIZE)))
-    await submitTransaction(_net)
-    const txId = await getSignedTxTransactionId(_net)
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Transaction submitted!`, PAD_END_SIZE)))
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`- Transaction ID: ${txId}`, PAD_END_SIZE)))
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`Submitting transaction...`, PAD_END_SIZE),
+      ),
+    );
+    await submitTransaction(_net);
+    const txId = await getSignedTxTransactionId(_net);
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Transaction submitted!`, PAD_END_SIZE),
+      ),
+    );
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`- Transaction ID: ${txId}`, PAD_END_SIZE),
+      ),
+    );
 
     console.log();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`Cleaning up transaction files...`, PAD_END_SIZE)))
+    console.log(
+      chalk.black.bgGreenBright.bold(
+        _.padEnd(`Cleaning up transaction files...`, PAD_END_SIZE),
+      ),
+    );
     await cleanupTransactionFiles();
-    console.log(chalk.black.bgGreenBright.bold(_.padEnd(`Done!`, PAD_END_SIZE)))
-
-
+    console.log(
+      chalk.black.bgGreenBright.bold(_.padEnd(`Done!`, PAD_END_SIZE)),
+    );
   } catch (error) {
-    console.error("ERROR:", error)
+    console.error("ERROR:", error);
     console.log();
     console.log();
-    console.log(chalk.white.bgRed.bold(_.padEnd('!', PAD_END_SIZE, '!')))
+    console.log(chalk.white.bgRed.bold(_.padEnd("!", PAD_END_SIZE, "!")));
     console.log(chalk.white.bgRed.bold(_.pad(error, PAD_END_SIZE)));
-    console.log(chalk.white.bgRed.bold(_.padEnd('!', PAD_END_SIZE, '!')))
+    console.log(chalk.white.bgRed.bold(_.padEnd("!", PAD_END_SIZE, "!")));
   }
 };
 
--- bin/jsonGenerator.js
+++ bin/jsonGenerator.js
@@ -1,9 +1,14 @@
-import dotenv from 'dotenv';
-import blake2 from 'blake2';
-import nacl from 'tweetnacl';
+import dotenv from "dotenv";
+import blake2 from "blake2";
+import nacl from "tweetnacl";
 import fs from "fs";
-import canonicalize from 'canonicalize';
-import * as util from 'tweetnacl-util';
+import canonicalize from "canonicalize";
+import * as util from "tweetnacl-util";
+import axios, { isAxiosError } from "axios";
+import _ from "lodash";
+import Ajv from "ajv";
+import schema from "./cip72Schema.json" assert { type: "json" };
+
 nacl.util = util;
 
 const MAX_CHARACTER_LENGTH = 64;
@@ -12,7 +17,7 @@ dotenv.config();
 
 const splitString = (value) => {
   if (value.length === 0) {
-    return [''];
+    return [""];
   }
 
   const substrings = [];
@@ -21,53 +26,88 @@ const splitString = (value) => {
   }
 
   return substrings;
-}
+};
 
-// **********************************************************************************************************
-// **********************************************************************************************************
-// **********************************************************************************************************
-
-const calculateRootHash = (cipFilePath) => {
-  const rawdata = fs.readFileSync(cipFilePath);
-  const dappMetadata = JSON.parse(rawdata);
-  const canonicalizedJson = canonicalize(dappMetadata)
-  const _hash = blake2.createHash('blake2b', { digestLength: 32 });
-  return _hash.update(Buffer.from(canonicalizedJson)).digest('hex')
-}
-
-// **********************************************************************************************************
-// **********************************************************************************************************
-// **********************************************************************************************************
-
-const generateMetadataJsonFile = (cipJsonFilePath, metadataFilePath, actionType, comment, metadataUrl, cipRootHash) => {
+const fetchMetadata = async (metadataUrl) => {
   try {
-    const rawdata = fs.readFileSync(cipJsonFilePath);
-    const { subject } = JSON.parse(rawdata);
-    const offChainStoragePathArray = splitString(metadataUrl)
+    console.log("Fetching metadata...");
+    const res = await axios.get(metadataUrl);
+    const metadata = res.data;
+    if (!_.isObject(metadata) || _.isEmpty(metadata)) {
+      throw new Error("Invalid metadata");
+    }
+    return metadata;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(`Request failed with status ${error.response.status}`);
+    } else {
+      throw error;
+    }
+  }
+};
+
+const validateMetadata = async (metadata) => {
+  const ajv = new Ajv({ strict: false });
+  const validate = ajv.compile(schema);
+  const valid = validate(metadata);
+  if (!valid) {
+    const errors = validate.errors;
+    throw new Error(
+      `Invalid metadata: ${errors.map(
+        (error) => `${error.instancePath} ${error.message}`,
+      )}`,
+    );
+  }
+  return true;
+};
+
+const calculateRootHash = async (metadataUrl) => {
+  try {
+    if (!metadataUrl) {
+      throw new Error("metadata url is required");
+    }
+    const dappMetadata = await fetchMetadata(metadataUrl);
+    await validateMetadata(dappMetadata);
+    const canonicalizedJson = canonicalize(dappMetadata);
+    const _hash = blake2.createHash("blake2b", { digestLength: 32 });
+    return {
+      metadata: dappMetadata,
+      rootHash: _hash.update(Buffer.from(canonicalizedJson)).digest("hex"),
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+const generateMetadataJsonFile = (
+  metadataFilePath,
+  actionType,
+  comment,
+  metadataUrl,
+  cipRootHash,
+  metadata,
+) => {
+  try {
+    const { subject } = metadata;
+    const offChainStoragePathArray = splitString(metadataUrl);
 
     const metadataJson = {
-      "1667": {
+      1667: {
         subject,
         rootHash: cipRootHash,
         metadata: offChainStoragePathArray,
         type: {
           action: actionType,
-          comment
+          comment,
         },
-      }
-    }
+      },
+    };
 
-    fs.writeFileSync(metadataFilePath, JSON.stringify(metadataJson))
-    return true
+    fs.writeFileSync(metadataFilePath, JSON.stringify(metadataJson));
+    return true;
   } catch (error) {
     return error;
   }
-}
+};
 
-// **********************************************************************************************************
-// **********************************************************************************************************
-// **********************************************************************************************************
-
-export {
-  calculateRootHash, generateMetadataJsonFile
-}
+export { calculateRootHash, generateMetadataJsonFile };
