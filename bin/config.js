8b37ac105244de61cd2fa43a1a5b8c368d126775
John Oshalusi <john.oshalusi@iohk.io>
2024-01-30 09:45:29 +0100

    chore: rename wallet address env variable to `PAYMENT_ADDRESS_FILE_PATH`

--- bin/config.js
+++ bin/config.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 export const protocolFilePath = process.env.PROTOCOL_FILE_PATH;
 export const paymentSKeyFilePath = process.env.PAYMENT_SKEY_FILE_PATH;
-export const walletAddress = fs.readFileSync(path.resolve(process.env.WALLET_ADDRESS_PATH), 'utf8');
+export const walletAddress = fs.readFileSync(
+  path.resolve(process.env.PAYMENT_ADDRESS_FILE_PATH),
+  'utf8',
+);
 export const net = process.env.NET;
 export const blockfrostApiKey = process.env.BLOCKFROST_API_KEY;
